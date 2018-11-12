e0e0a2f1e309d69e818279827c9da8e9c5e676cd
Aleksandr Kotlyar <akotlyar@ctcmedia.ru>
2018-11-12 23:44:12 +0300

    add convert to Double and cut currency

--- src/test/java/StringCutterTest.java
+++ src/test/java/StringCutterTest.java
@@ -1,6 +1,7 @@
 import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
 
+import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.List;
 import java.util.logging.Logger;
@@ -11,37 +12,47 @@ import static org.junit.Assert.assertEquals;
  * Created by aleksandr.kotlyar on 12.11.2018. 22:10
  */
 public class StringCutterTest {
-    private static final String plusDotSpace = "+55.9 RUB";
-    private static final String minusDotSpace = "-55.9 RUB";
-    private static final String commaSpace = "55,9 RUB";
-    private static final String dotTwoSpaces = "55.9  RUB";
-    private static final String dotTabSpace = "55.9  \t RUB";
-    private static final String dotNewLine = "55.9 \nRUB";
-    private static final String minusSpaceDotSpace = "- 55.9 RUB";
+    private static final String plusDotSpace = "+55.99 RUB";
+    private static final String minusDotSpace = "-55.99 RUB";
+    private static final String commaSpace = "55,99 RUB";
+    private static final String dotTwoSpaces = "55.99  RUB";
+    private static final String dotTabSpace = "55.99  \t RUB";
+    private static final String dotNewLine = "55.99 \nRUB";
+    private static final String minusSpaceDotSpace = "- 55.99 RUB";
+    private static final String minusSpaceDotCommaSpace = "- 55.99, RUB";
     private List<String> prices = Arrays.asList(plusDotSpace,
             minusDotSpace,
             commaSpace,
             dotTwoSpaces,
             dotTabSpace,
             dotNewLine,
-            minusSpaceDotSpace);
+            minusSpaceDotSpace,
+            minusSpaceDotCommaSpace);
 
     @Test
     public void testSubstring() {
-        String fixPrice = "55,9RUB";
+        BigDecimal fixPrice = new BigDecimal(55.99).setScale(2, BigDecimal.ROUND_DOWN);
         for (String price : prices) {
-            info("unParsed price= '" + price + "'");
-            price = StringUtils.remove(price, "+");
-            price = StringUtils.remove(price, "-");
-            price = StringUtils.deleteWhitespace(price);
-            price = StringUtils.replaceOnce(price, ".", ",");
-            info("price = '" + price + "', fix = '55,9RUB'");
-            assertEquals(price, fixPrice);
+            BigDecimal parsedPrice = convertToDecimalPriceWithoutShit(price);
+            assertEquals(parsedPrice, fixPrice);
         }
     }
 
+    private BigDecimal convertToDecimalPriceWithoutShit(String price) {
+        info("unParsedPrice= '" + price + "'");
+        price = StringUtils.remove(price, "+");
+        price = StringUtils.remove(price, "-");
+        price = StringUtils.deleteWhitespace(price);
+        price = StringUtils.remove(price, "RUB");
+        price = StringUtils.removeEnd(price, ",");
+        price = StringUtils.replaceOnce(price, ",", ".");
+        info("parsedPrice = '" + price + "'");
+        return new BigDecimal(price).setScale(2, BigDecimal.ROUND_DOWN);
+    }
+
     private void info(String logMessage) {
-        getLoggerInstance().info("\n--------------------------------\n" + logMessage + "\n--------------------------------\n");
+        String s = "\n--------------------------------\n";
+        getLoggerInstance().info( s + logMessage + s);
     }
 
     private static final Logger LOGGER = Logger.getLogger(StringCutterTest.class.getName());
